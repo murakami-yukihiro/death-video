1db92c9c5019ff9f9b673319f5d268b9b0e93a18
Murakami Yukihiro <murakamiyukihiro@realglobe.jp>
2021-10-12 19:47:25 +0900

    initial commit.

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { StrictMode } from 'react';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
   document.getElementById('root')
 );
 
