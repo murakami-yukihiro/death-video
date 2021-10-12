95a583bbfa5b3649f053b215b3d730bd2d1435b1
Murakami Yukihiro <murakamiyukihiro@realglobe.jp>
2021-10-12 23:30:43 +0900

    add tailwindcss and react-app-rewired

---
+++ config-overrides.js
@@ -0,0 +1,26 @@
+/* config-overrides.js */
+
+const { override, addPostcssPlugins } = require('customize-cra');
+
+module.exports = {
+  webpack: override(
+    addPostcssPlugins([require('tailwindcss'), require('autoprefixer')])
+  ),
+  // Extend/override the dev server configuration used by CRA
+  // See: https://github.com/timarney/react-app-rewired#extended-configuration-options
+  devServer: function (configFunction) {
+    return function (proxy, allowedHost) {
+      // Create the default config by calling configFunction with the proxy/allowedHost parameters
+      // Default config: https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/config/webpackDevServer.config.js
+      const config = configFunction(proxy, allowedHost);
+
+      // Set loose allow origin header to prevent CORS issues
+      config.headers = {
+        'Cross-Origin-Embedder-Policy': 'require-corp',
+        'Cross-Origin-Opener-Policy': 'same-origin',
+      };
+
+      return config;
+    };
+  },
+};
